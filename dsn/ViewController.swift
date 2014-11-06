b352afe1a799238a3b3aab1bb683a50c952233bd
Maxime Bedard <maxim3.bedard@gmail.com>
2014-11-06 18:27:52 -0500

    Initial Commit

---
+++ dsn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dsn
+//
+//  Created by Maxime Bédard on 2014-11-06.
+//  Copyright (c) 2014 etsmtl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
