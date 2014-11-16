2082ac734ea9c7e226ef783445d44bd32af1eca8
Charles Levesque <dreamtooloverture@gmail.com>
2014-11-15 23:26:01 -0500

    Added and finished design of ProfileViewController.

---
+++ dsn/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  dsn
+//
+//  Created by Charles Levesque on 2014-11-15.
+//  Copyright (c) 2014 etsmtl. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController{
+    @IBOutlet weak var btnEnregistrer: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
