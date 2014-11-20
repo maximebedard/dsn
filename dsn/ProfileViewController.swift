7e3f8a2e0a9ab5e976a71a623af81db76f67ca27
Charles Levesque <dreamtooloverture@gmail.com>
2014-11-20 15:48:55 -0500

    CocoaPods est configurer avec le projet. SWRevealViewController a été ajouté comme dépendance via CocoaPods. Menu fonctionnel. Ajout d'une image pour le menu dans la navigation.

--- dsn/ProfileViewController.swift
+++ dsn/ProfileViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class ProfileViewController: UIViewController{
     @IBOutlet weak var btnEnregistrer: UIButton!
+    @IBOutlet weak var btnMenu: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.menuInit()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func menuInit() {
+        if let swController: SWRevealViewController = self.revealViewController(){
+            self.btnMenu.target = self.revealViewController()
+            self.btnMenu.action = NSSelectorFromString("revealToggle:")
+            self.navigationController?.navigationBar.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
 }
---
+++ dsn/dsn-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  dsn-Bridging-Header.h
+//  dsn
+//
+//  Created by Charles Levesque on 2014-11-18.
+//  Copyright (c) 2014 etsmtl. All rights reserved.
+//
+
+#ifndef dsn_dsn_Bridging_Header_h
+#define dsn_dsn_Bridging_Header_h
+
+#import <SWRevealViewController/SWRevealViewController.h>
+
+#endif
